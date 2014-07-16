f8beb3e0ddbac6c5e093b59dc4d49656082f9fd9
AntonKolmakov <kolmakoff@icloud.com>
2014-07-16 22:00:02 +0400

    clear

--- sequence.rb
+++ sequence.rb
@@ -1,33 +1,6 @@
 # #!/usr/bin/ruby
-# require 'pry'
-# class Sequence
-#   def initialize(arg)
-#     the_sequence(arg)
-#   end
 
-#   def the_sequence(arg)
-#     counts = Hash.new(0)
-#     @total = []
-#     arr = arg.to_s.split('')
-#     i = 0
-#     while i < 1 do
-#       for item in arr
-#         counts.has_key?(item) && counts[item] ? counts[item] += 1 : counts[item] += 1
-#         binding.pry
-#       end
-#       counts.each do |k, v|
-#         @total << v
-#         @total << k
-#       end
-#       p @total.join()
-#       p arr = @total.join().split('')
-#       counts.clear
-#       i += 1
-#     end
-#   end
-# end
-
-# Sequence.new(1211)
+require 'pry'
 
 module Enumerable
   def cluster
